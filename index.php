83d1623f1ccf512e477b1384a3b36388e7338136
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-12 08:40:42 -0300

    portifolio total

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 <!DOCTYPE HTML>
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Início</title>
-    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
-</head>
-<body>
-    
-    <div class="container mt-3">
-        <div class="row">
-            <div class="col">
-                <a href="paginas/login.php"><button class="btn btn-outline-primary">Login</button></a> 
+    <head>
+        <meta charset="UTF-8">
+        <title>Início</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row mt-3">
+                <div class="col">
+                    <a href="paginas/login.php"><button class="btn btn-outline-primary">Login</button></a> 
+                </div>
+                <div class="col">
+                    <a href="plugins/hacking-minigame/dist/index.html"><button class="btn btn-outline-secondary">Hacking Minigame</button></a>
+                </div>
+                <div class="col">
+                    <a href="plugins/watchdogs-hacking-app/dist/index.html"><button class="btn btn-outline-success">Watch Dogs</button></a>
+                </div>
             </div>
-            <div class="col">
-                <a href="plugins/hacking-minigame/dist/index.html"><button class="btn btn-outline-secondary">Hacking Minigame</button></a>
+
+            <div class="row mt-3">
+                <div class="col">
+                    <a href="pluginsdeterceiros/Hermes-plugin/index.html"><button class="btn btn-outline-danger">Hermes Plugin</button></a>
+                </div>
+                <div class="col">
+                    <a href="pluginsdeterceiros/p1/dist/index.html"><button class="btn btn-outline-dark">Manu Plugin</button></a>
+                </div>
             </div>
-            <div class="col">
-                <a href="plugins/watchdogs-hacking-app/dist/index.html"><button class="btn btn-outline-success">Watch Dogs</button></a>
-            </div>
+
         </div>
-        
-    </div>
-    
-</body>
+
+    </body>
 </html>
